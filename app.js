65651f3d62e97aa477a19906b6615f6e22adf8b2
최윤석 <yunsuk2yo@gmail.com>
2026-06-14 15:30:13 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,42 @@
+const map = L.map('map').setView(
+[37.48028,127.01324],
+15
+);
 
+L.tileLayer(
+'https://tile.openstreetmap.org/{z}/{x}/{y}.png'
+).addTo(map);
+
+let marker;
+
+navigator.geolocation.watchPosition(
+(pos)=>{
+
+const lat = pos.coords.latitude;
+const lng = pos.coords.longitude;
+
+if(marker){
+marker.setLatLng([lat,lng]);
+}else{
+marker = L.marker([lat,lng])
+.addTo(map);
+}
+
+map.setView([lat,lng]);
+
+document.getElementById("speed")
+.innerHTML =
+"속도 : " +
+Math.round(
+(pos.coords.speed || 0)*3.6
+)
++" km/h";
+
+},
+(err)=>{
+console.log(err);
+},
+{
+enableHighAccuracy:true
+}
+);
