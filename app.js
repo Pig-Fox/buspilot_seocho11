16b36d2d8a3179ea38c091e9e9c6481a3ed2c1e4
최윤석 <yunsuk2yo@gmail.com>
2026-06-19 22:44:34 +0900

    app.js 업데이트

--- app.js
+++ app.js
@@ -17,5 +17,31 @@ map.on('click', function(e){
         '위도 : ' + lat +
         '\n경도 : ' + lng
     );
+map.on('click', function(e){
 
+    const lat = e.latlng.lat.toFixed(6);
+    const lng = e.latlng.lng.toFixed(6);
+
+    const stopName = prompt(
+        '정류장명을 입력하세요',
+        ''
+    );
+
+    L.popup()
+        .setLatLng(e.latlng)
+        .setContent(
+            '<b>정류장 정보</b><br><br>' +
+            '정류장명 : ' + (stopName || '미입력') + '<br><br>' +
+            '위도 : ' + lat + '<br>' +
+            '경도 : ' + lng + '<br><br>' +
+            'stops.json 입력용<br><br>' +
+            '{<br>' +
+            '"name": "' + (stopName || '') + '",<br>' +
+            '"lat": ' + lat + ',<br>' +
+            '"lng": ' + lng + '<br>' +
+            '}'
+        )
+        .openOn(map);
+
+});
 });
