d5617e50d71b1eb541aca3426c456e4ab5c4113c
최윤석 <yunsuk2yo@gmail.com>
2026-06-19 22:48:48 +0900

    app.js 업데이트

--- app.js
+++ app.js
@@ -1,22 +1,3 @@
-const map = L.map('map').setView([37.490, 127.022], 14);
-
-L.tileLayer(
-'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-{
-    maxZoom: 20
-}
-).addTo(map);
-
-// 지도 클릭 시 좌표 표시
-map.on('click', function(e){
-
-    const lat = e.latlng.lat.toFixed(6);
-    const lng = e.latlng.lng.toFixed(6);
-
-    alert(
-        '위도 : ' + lat +
-        '\n경도 : ' + lng
-    );
 map.on('click', function(e){
 
     const lat = e.latlng.lat.toFixed(6);
@@ -43,5 +24,4 @@ map.on('click', function(e){
         )
         .openOn(map);
 
-});
 });
